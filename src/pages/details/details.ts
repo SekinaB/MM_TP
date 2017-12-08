37b931f719ffe076cf9aad9a1c57afa25f145ebe
Sekina <kina74@hotmail.fr>
2017-12-08 15:23:38 +0100

    TP1 fin

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+//import { Result } from '../home/home';
 
 /**
  * Generated class for the DetailsPage page.
@@ -7,14 +8,29 @@ import { NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+//export interface Result {
+//  author: string;
+//  date: number;
+//  image: string;
+//  title: string
+//}
+
 
 @Component({
   selector: 'page-details',
   templateUrl: 'details.html',
 })
 export class DetailsPage {
-
+  author: string;
+  date: number;
+  image: string;
+  title: string;
+  //currentResult: Result;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.author = this.navParams.get('author');
+    this.title = this.navParams.get('title');
+    this.date = this.navParams.get('date');
+    this.image = this.navParams.get('image');
   }
 
   ionViewDidLoad() {
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,38 +1,43 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { DetailsPage } from '../details/details';
 
 export interface Result {
   author: string;
   date: number;
   image: string;
-  title: string;
+  title: string
 }
 
 const fakeResults: Result[] = [{
   author: 'Author 1',
   date: 2017,
   image:'https://cdn-images-1.medium.com/max/1600/1*RxmPeSybIUtxea4Vh98y2g.jpeg',
-  title: 'Fuck this shit '
+  title: 'Fuck this shit ',
   }, {
   author: 'Author 2',
   date: 2015,
   image: 'http://memions.com/wp-content/themes/wumblr/themify/img.php?src=http://memions.com/wp-content/uploads/2015/02/10929203_781076021976935_808464437295772567_n.jpg&w=590&h=&q=70',
-  title: 'About life'
+  title: 'About life',
   }, {
   author: 'Author 3',
   date: 2018,
   image: 'https://ih1.redbubble.net/image.202504306.6869/sticker,375x360-bg,ffffff.u2.png',
-  title: 'Things happen'
+  title: 'Things happen',
   }]
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
   fakeResults : Result[] ;
+  detailsPage : any ;
+  params : Object ;
   constructor(public navCtrl: NavController) {
-      this.fakeResults = [];
+      this.fakeResults = [] ;
+      this.detailsPage = DetailsPage;
+      this.params = {}
   }
 
   getResults($event : any) : void {
@@ -44,4 +49,5 @@ export class HomePage {
       this.fakeResults = [];
     }
   }
+
 }
